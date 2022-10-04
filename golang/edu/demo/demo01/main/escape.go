741ec6f900e65159a995d365f6aa75e71808636a
alannchen <alannchen@tencent.com>
2022-10-04 18:53:14 +0800

    update

---
+++ golang/edu/demo/demo01/main/escape.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("tom\tjack")
+
+	fmt.Println("hello\nworld")
+
+	fmt.Println("C:\\usr\\xxx\\xxx")
+
+	fmt.Println("TOM: \"i love you\"")
+}
